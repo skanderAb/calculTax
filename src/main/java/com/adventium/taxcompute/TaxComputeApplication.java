4c6765b81415e1544ffdee5e203c3aa20d1a4b12
Skander Abdeljelil <skandar.abdeljelil@esprit.tn>
2021-12-18 10:13:26 +0100

    Creating the SpringBoot application 1st commit

---
+++ src/main/java/com/adventium/taxcompute/TaxComputeApplication.java
@@ -0,0 +1,13 @@
+package com.adventium.taxcompute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxComputeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxComputeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adventium/taxcompute/TaxComputeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventium.taxcompute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxComputeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
